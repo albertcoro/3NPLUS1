f8d9afe9179e5e1788593d45b8a1b1be33d20346
Albert Corominas Mairscot <38464645+albertcoro@users.noreply.github.com>
2022-06-05 21:21:00 +0200

    Add files via upload

---
+++ 3n+1.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot
+
+minimum = 0
+maximum = 100
+x = list()
+y = list()
+
+for i in range(minimum,maximum):
+    j = i
+    count = 0
+    while j>1:
+        x.append(count)
+        y.append(j)
+        if j % 2 == 0: #Is a pair number
+            j = j /2
+        else: #Is an odd number
+            j = (3*j)+1
+        count = count +1
+    
+    pyplot.plot(x, y, color='black')
+    #print(i," took ",count," steps until reaching 1")
+ 
+pyplot.show() 
